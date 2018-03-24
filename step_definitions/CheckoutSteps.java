de7c02fd50b861734f073f4e2e8b857911c12934
pbhupen01 <pandeybhupen01@gmail.com>
2018-03-24 06:31:41 +0530

    Basic implementation of cucumber test

---
+++ step_definitions/CheckoutSteps.java
@@ -0,0 +1,32 @@
+package step_definitions;
+
+import cucumber.api.java.en.*;
+import cucumber.api.PendingException;
+import implementation.Checkout;
+import org.junit.Assert;
+import java.util.Map;
+import java.util.HashMap;
+
+public class CheckoutSteps {
+	
+	Map<String, Integer> priceMap = new HashMap<>();
+	Checkout checkout = new Checkout(); ;
+	
+	@Given("^the price of a \"([^\"]*)\" is (\\d+)c$")
+public void the_price_of_a_is_c(String name, int price) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    priceMap.put(name, price);
+}
+
+@When("^I checkout (\\d+) \"([^\"]*)\"$")
+public void i_checkout(int itemCount, String itemName) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions    
+    checkout.add(itemCount, priceMap.get(itemName));
+}
+
+@Then("^the total price should be (\\d+)c$")
+public void the_total_price_should_be_c(int total) throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+    Assert.assertEquals(total, checkout.total());
+}
+}
